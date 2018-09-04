432a2fbdd9d4a96b5bc212a4b9c0a46bc88ce510
tpham20908 <tpham20908@gmail.com>
2018-09-03 20:31:16 -0400

    added constrains GraphQLNonNull to all fields

--- graphql-playlist/server/schema/schema.js
+++ graphql-playlist/server/schema/schema.js
@@ -10,7 +10,8 @@ const {
 	GraphQLSchema,
 	GraphQLID,
 	GraphQLInt,
-	GraphQLList
+	GraphQLList,
+	GraphQLNonNull
 } = graphql;
 
 // dummy data
@@ -47,6 +48,7 @@ const BookType = new GraphQLObjectType({
 			type: AuthorType,
 			resolve(parent, args) {
 				// return _.find(authors, {id: parent.authorId});
+				return Author.findById(parent.authorId);
 			}
 		}
 	})
@@ -62,6 +64,7 @@ const AuthorType = new GraphQLObjectType({
 			type: new GraphQLList(BookType),
 			resolve(parent, args) {
 				// return _.filter(books, {authorId: parent.id});
+				return Book.find({authorId: parent.id});
 			}
 		}
 	})
@@ -85,6 +88,7 @@ const RootQuery = new GraphQLObjectType({
 			resolve(parent, args) {
 				// code to get data from db / other source
 				// return _.find(books, {id: args.id});
+				return Book.findById(args.id);
 			}
 		},
 		author: {
@@ -92,18 +96,21 @@ const RootQuery = new GraphQLObjectType({
 			args: { id: {type: GraphQLID}},
 			resolve(parent, args) {
 				// return _.find(authors, {id: args.id});
+				return Author.findById(args.id);
 			}
 		},
 		books: {
 			type: new GraphQLList(BookType),
 			resolve(parent, args) {
 				// return books;
+				return Book.find({});
 			}
 		},
 		authors: {
 			type: new GraphQLList(AuthorType),
 			resolve(parent, args) {
 				// return authors;
+				return Author.find({});
 			}
 		},
 		shipment: {
@@ -111,12 +118,14 @@ const RootQuery = new GraphQLObjectType({
 			args: { id: {type: GraphQLID}},
 			resolve(parent, args) {
 				// return _.find(shipments, {id: args.id});
+				return Shipment.findById(args.id);
 			}
 		},
 		shipments: {
 			type: new GraphQLList(ShipmentType),
 			resolve(parent, args) {
 				// return shipments;
+				return Shipment.find({});
 			}
 		}
 	}
@@ -128,8 +137,8 @@ const Mutation = new GraphQLObjectType({
 		addAuthor: {
 			type: AuthorType,
 			args: {
-				name: {type: GraphQLString},
-				age: {type: GraphQLInt}
+				name: {type: new GraphQLNonNull(GraphQLString)},
+				age: {type: new GraphQLNonNull(GraphQLInt)}
 			},
 			resolve(parent, args) {
 				let author = new Author({
@@ -139,11 +148,27 @@ const Mutation = new GraphQLObjectType({
 				return author.save();
 			}
 		},
+		addBook: {
+			type: BookType,
+			args: {
+				name: {type: new GraphQLNonNull(GraphQLString)},
+				genre: {type: new GraphQLNonNull(GraphQLString)},
+				authorId: {type: new GraphQLNonNull(GraphQLID)}
+			},
+			resolve(parent, args) {
+				let book = new Book({
+					name: args.name,
+					genre: args.genre,
+					authorId: args.authorId
+				});
+				return book.save();
+			}
+		},
 		addShipment: {
 			type: ShipmentType,
 			args: {
-				senderName: {type: GraphQLString},
-				receiverName: {type: GraphQLString}
+				senderName: {type: new GraphQLNonNull(GraphQLString)},
+				receiverName: {type: new GraphQLNonNull(GraphQLString)}
 			},
 			resolve(parent, args) {
 				let shipment = new Shipment({
